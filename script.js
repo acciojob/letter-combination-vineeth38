8dc8a0dd91c06b78f65e42147b821031147e0fc8
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-03-31 16:02:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-	const L = {'2':"abc",'3':"def",'4':"ghi",'5':"jkl",
-     '6':"mno",'7':"pqrs",'8':"tuv",'9':"wxyz"}
-
-var letterCombinations = function(D) {
-    let len = D.length, ans = []
-    if (!len) return []
-    const bfs = (pos, str) => {
-        if (pos === len) ans.push(str)
-        else {
-            let letters = L[D[pos]]
-            for (let i = 0; i < letters.length; i++)
-                bfs(pos+1,str+letters[i])
-        }
-    }
-    bfs(0,"")
-    return ans
-};
+function letterCombinations(digits) {
+if (!digits) {
+return [];
 }
 
+const mappings = {
+'2': 'abc',
+'3': 'def',
+'4': 'ghi',
+'5': 'jkl',
+'6': 'mno',
+'7': 'pqrs',
+'8': 'tuv',
+'9': 'wxyz'
+};
+
+const results = [];
+
+function generateCombinations(currentIndex, currentString) {
+if (currentIndex === digits.length) {
+results.push(currentString);
+return;
+}
+
+const currentDigit = digits[currentIndex];
+const letters = mappings[currentDigit];
+
+for (let i = 0; i < letters.length; i++) {
+const letter = letters[i];
+generateCombinations(currentIndex + 1, currentString + letter);
+}
+}
+
+generateCombinations(0, '');
+
+return results.sort();
+}
 module.exports = letterCombinations;
